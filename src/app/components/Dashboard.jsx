5437495a8111062fa79a50298c776c49c292cecb
charlesmarvin <marvin.charles@gmail.com>
2015-12-19 10:08:10 -0400

    more minor changes to chart colors

--- src/app/components/Dashboard.jsx
+++ src/app/components/Dashboard.jsx
@@ -29,18 +29,12 @@ const baseChartConfig = {
                 distance: -40
             },
             colors: [
-                '#A6CEE3',
-                '#1F78B4',
-                '#B2DF8A',
-                '#33A02C',
-                '#FB9A99',
-                '#E31A1C',
-                '#FDBF6F',
-                '#FF7F00',
-                '#CAB2D6',
-                '#6A3D9A',
-                '#FFFF99',
-                '#B15928'
+            // colours selected using http://colorbrewer2.org
+                '#e41a1c',
+                '#377eb8',
+                '#4daf4a',
+                '#984ea3',
+                '#ff7f00'
             ]    
         }
     },
