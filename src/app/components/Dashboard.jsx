01f1fc961948faee5fc87e5a975b5cdf1734ca97
charlesmarvin <marvin.charles@gmail.com>
2015-12-19 09:53:23 -0400

    Minor changes to chart colors

--- src/app/components/Dashboard.jsx
+++ src/app/components/Dashboard.jsx
@@ -27,18 +27,25 @@ const baseChartConfig = {
             dataLabels: {
                 enabled: true,
                 distance: -40
-            }    
+            },
+            colors: [
+                '#A6CEE3',
+                '#1F78B4',
+                '#B2DF8A',
+                '#33A02C',
+                '#FB9A99',
+                '#E31A1C',
+                '#FDBF6F',
+                '#FF7F00',
+                '#CAB2D6',
+                '#6A3D9A',
+                '#FFFF99',
+                '#B15928'
+            ]    
         }
     },
     series: [{
         name: 'Students',
-        colors: [
-            '#C83737',
-            '#2F4986',
-            '#17becf',
-            '#C87937',
-            '#bcbd22'
-        ],
         data: []
     }]
 };
